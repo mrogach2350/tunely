49b70ef920e30669a229fd6bbc264ef398978f75
mrogach2350 <m.rogach@gmail.com>
2016-09-06 16:03:58 -0700

    starts sprint 3

--- models/album.js
+++ models/album.js
@@ -1,11 +1,13 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+module.exports.Song = require('./song.js');
 
 var AlbumSchema = new Schema({
   name: String,
   artistName: String,
   releaseDate: String,
-  genres: Array
+  genres: Array,
+  songs = []
 });
 
 var Album = mongoose.model('Album', AlbumSchema);
